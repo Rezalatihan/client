b1afdf3b0452e5c9a976e1b159e80f1253304b9c
unknown <rezacatria1@gmail.com>
2022-12-25 01:43:10 +0700

    pindah api

--- src/app/api/address.js
+++ src/app/api/address.js
@@ -12,7 +12,7 @@ export const getAddress = async () => {
 }
 
 export const getLocation = async (lokasi, kodeInduk) => {
-  return await axios.get(`https://regions-indoneisa.herokuapp.com/api/${lokasi}?kode_induk=${kodeInduk}`);
+  return await axios.get(`https://server-atihan-production.up.railway.app/${lokasi}?kode_induk=${kodeInduk}`);
 }
 
 export const createAddress = async data => {
@@ -24,3 +24,30 @@ export const createAddress = async data => {
     }
   })
 }
+
+
+
+
+// import axios from "axios"
+
+
+// export const registerUser = async (data) => {
+//   return await axios.post(`https://server-atihan-production.up.railway.app/auth/register`, data);
+// }
+
+// export const loginUser = async (data) => {
+//   return await axios.post(`https://server-atihan-production.up.railway.app/auth/login`, data);
+// }
+
+// export const logoutUser = async () => {
+//   let { token } = localStorage.getItem('auth') ? JSON.parse(localStorage.getItem('auth')) : {};
+
+//   return await axios.post(`https://server-atihan-production.up.railway.app/auth/logout`, null, {
+//     headers: {
+//       authorization: `Bearer ${token}`
+//     }
+//   }).then(res => {
+//     localStorage.removeItem('auth');
+//     return res
+//   });
+// }
